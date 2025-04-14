8418fd38155c4be5663109246bff47836513e0ba
ermills <79326171+ermills@users.noreply.github.com>
2025-04-14 17:51:22 -0500

    Update leagueInfo.js
    Updated Eric Mills favorite team

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -30,7 +30,7 @@ export const managers = [
     "bio": "God.",
     "photo": "/managers/eric-mills.jpg", // square ratio recommended (no larger than 500x500)
     "fantasyStart": 2006, // (optional) when did the manager start playing fantasy football
-    "favoriteTeam": "msp", // (optional) favoritYoue NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+    "favoriteTeam": "min", // (optional) favoritYoue NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
     "rival": {
       name: "Farts", // Can be anything (usually your rival's name)
