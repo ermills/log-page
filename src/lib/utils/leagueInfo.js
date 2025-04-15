6c74c3fccb60a0983df1381bd6a4b2b58129748a
ermills <79326171+ermills@users.noreply.github.com>
2025-04-15 10:13:04 -0500

    Update leagueInfo.js
    Added blog

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "1205320827772350464"; // your league ID
 export const leagueName = "League of Ordinary Gentlemen"; // your league name
 export const dues = 30; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
